eef176b28c18da54e29940b30b37b1915f31c7b3
Nataliya Korobitsina <toropova@chemomsu.ru>
2026-09-24 16:38:23 +0600

    меню проработали

--- JS/script.js
+++ JS/script.js
@@ -4,13 +4,28 @@ document.addEventListener('DOMContentLoaded', function() {
         const navbarCollapsible = document.body.querySelector('#mainNav');
 
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+            navbarCollapsible.classList.remove('navbar-shrink');
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            navbarCollapsible.classList.add('navbar-shrink');
         }
+
+        const links = document.querySelectorAll('.nav-link'); //ищем все навигационные ссылки
+        const sections = document.querySelectorAll('section'); //ищем все секции
+
+        sections.forEach(section => { //для каждой секции
+            if (window.scrollY >= (section.offsetTop - 100)) { //проверяем, если стр прокручена больше, чем расстояние секции от начала стр
+                console.log(window.scrollY + " >= " + section.offsetTop + " " + section.id); //отладка, удалить
+                links.forEach(link => { //для каждой ссылки
+                    link.classList.remove('active') //удаляем активный класс
+                    if (link.href.split('#').pop() === section.id) { //проверяем, если href ссылки без # === id екции
+                        link.classList.add('active') //добавляем ссылке активный класс
+                    }
+                })
+            }
+        })
     }
 
-    
+
 
     navInit();
     window.addEventListener('scroll', () => {
